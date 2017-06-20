85e058a706fd9a5238f13121485e0ed0410da77e
Jan Vandepitte <jan.vandepitte@centric.eu>
2017-06-20 13:11:07 +0200

    modif

--- src/visualizing_category_theory/core.cljs
+++ src/visualizing_category_theory/core.cljs
@@ -18,23 +18,25 @@
 
 (defn update-state [state]
   ; Update sketch state by changing circle color and position.
-  {:color (mod (+ (:color state) 0.7) 255)
-   :angle (+ (:angle state) 0.1)})
+  ;{:color (mod (+ (:color state) 0.7) 255)
+  ; :angle (+ (:angle state) 0.1)})
+  state)
 
 (defn draw-state [state]
   ; Clear the sketch by filling it with light-grey color.
   (q/background 240)
   ; Set circle color.
   (q/fill (:color state) 255 255)
+  (q/ellipse 10 10 10 10)
   ; Calculate x and y coordinates of the circle.
-  (let [angle (:angle state)
-        x (* 150 (q/cos angle))
-        y (* 150 (q/sin angle))]
+  ;(let [angle (:angle state)
+  ;      x (* 150 (q/cos angle))
+  ;      y (* 150 (q/sin angle))]
     ; Move origin point to the center of the sketch.
-    (q/with-translation [(/ (q/width) 2)
-                         (/ (q/height) 2)]
+    ;(q/with-translation [(/ (q/width) 2)
+    ;                     (/ (q/height) 2)]
       ; Draw the circle.
-      (q/ellipse x y 100 100))))
+    ;  (q/ellipse x y 100 100))))
 
 (q/defsketch visualizing_category_theory
   :host "visualizing_category_theory"
